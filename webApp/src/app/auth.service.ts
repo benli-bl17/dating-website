acd97c0dc615db1b1557e0689eadf8a763a74b50
fredli53016 <fredli53016@gmail.com>
2018-10-02 21:36:37 +1000

    Implementing logout

--- webApp/src/app/auth.service.ts
+++ webApp/src/app/auth.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
 
   private _loginUrl = "http://localhost:3000/api/login"
 
   private _registerUrl = "http://localhost:3000/api/register"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private _router:Router) { }
 
   loginUser(user) {
     return this.http.post<any>(this._loginUrl, user)
+  }
+  logoutUser(){
+    localStorage.removeItem('token')
+    this._router.navigate(['/events'])
   }
 
   registerUser(user) {
--- webApp/src/app/login/login.component.ts
+++ webApp/src/app/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
         res => {
           console.log(res)
           localStorage.setItem('token', res.token)
-          this._router.navigate(['/events'])
+          this._router.navigate(['/members'])
         },
         err => console.log(err)
       )
