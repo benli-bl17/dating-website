3f1c7e8a5ecefec97a104207ef69ab36453b438b
fredli53016 <fredli53016@gmail.com>
2018-10-02 15:45:33 +1000

    verifyToken

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,6 +15,21 @@ mongoose.connect(db, err => {
         console.log('Connected to Mogodb')
     }
 })
+function verifyToken(req, res, next) {
+    if(!req.headers.authorization) {
+      return res.status(401).send('Unauthorized request')
+    }
+    let token = req.headers.authorization.split(' ')[1]
+    if(token === 'null') {
+      return res.status(401).send('Unauthorized request')    
+    }
+    let payload = jwt.verify(token, 'aip')
+    if(!payload) {
+      return res.status(401).send('Unauthorized request')    
+    }
+    req.userId = payload.subject
+    next()
+  }
 
 router.get('/', (req, res) => {
     res.send('From API route')
