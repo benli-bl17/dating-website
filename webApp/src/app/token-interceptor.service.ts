48eabdcb8c300581bf4c2c44d43206a4b2f05315
fredli53016 <fredli53016@gmail.com>
2018-10-02 15:42:05 +1000

    Interjector

--- webApp/src/app/token-interceptor.service.ts
+++ webApp/src/app/token-interceptor.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import{ HttpInterceptor } from '@angular/common/http'
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class TokenInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private injector: Injector) { }
 
   intercept(req,next){
+    let authService = this.injector.get(AuthService)
     let tokenizedReq = req.clone({
       setHeaders:{
-        Authorization:'Bearer xx.yy.zz'
+        Authorization:`Bearer ${authService.getToken()}`
       }
     })
     return next.handle(tokenizedReq)
