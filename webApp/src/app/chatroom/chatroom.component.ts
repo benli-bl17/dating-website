fc1544cdf00878467453dfd39b2c7e3fcbd79812
fredli53016 <fredli53016@gmail.com>
2018-10-09 17:56:31 +1100

    chatroom

--- webApp/src/app/chatroom/chatroom.component.ts
+++ webApp/src/app/chatroom/chatroom.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { WebsocketService} from "../websocket.service";
 import {UserInfoService} from "../user-info.service";
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-chatroom',
@@ -14,22 +16,30 @@ export class ChatroomComponent implements OnInit {
   userInfoData=[];
   user = "";
   constructor(private websocket: WebsocketService,
-              private _userInfoService:UserInfoService) { }
+              private _userInfoService:UserInfoService,
+              private _authService:AuthService,
+              private _router:Router) { }
 
   sendMessage(user){
     this.websocket.sendMessage(user+ ":       " + this.message);
-    this.message = '';
+     this.message = '';
+    
   }
 
   ngOnInit() {
-    this.connection = this.websocket.getMessages().subscribe(message =>{
-      this.messages.push(message);
-    })
-    this._userInfoService.getUserInfo()
-      .subscribe(
-        res => this.userInfoData = res,
-        err => console.log(err)
-      )
+    if(this._authService.loggedIn()){
+      this.connection = this.websocket.getMessages().subscribe(message =>{
+        this.messages.push(message);
+      })
+      this._userInfoService.getUserInfo()
+        .subscribe(
+          res => this.userInfoData = res,
+          err => console.log(err)
+        )
+    } else {
+      this._router.navigate(['/login'])
+    }
+
   }
 
   ngOnDestory(){
